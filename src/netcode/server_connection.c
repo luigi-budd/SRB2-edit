63981051ba9e5640585782210b67222654e0fbaa
romoney5 <whygetmugged@gmail.com>
2025-11-28 17:22:40 -0500

    Revert "Fix file checking not working"
    This reverts commit c034617cbfdddda685730a01edf59953169d193d.
    
    Oh

--- src/netcode/server_connection.c
+++ src/netcode/server_connection.c
@@ -496,12 +496,10 @@ void PT_TellFilesNeeded(SINT8 node)
 {
 	if (server && serverrunning)
 	{
-		doomdata_t *netbuffer = DOOMCOM_DATA(doomcom);
 		UINT8 *p;
 		INT32 firstfile = netbuffer->u.filesneedednum;
-		doomcom = D_NewPacket(PT_MOREFILESNEEDED, node, 0);
-		netbuffer = DOOMCOM_DATA(doomcom);
 
+		netbuffer->packettype = PT_MOREFILESNEEDED;
 		netbuffer->u.filesneededcfg.first = firstfile;
 		netbuffer->u.filesneededcfg.more = 0;
 

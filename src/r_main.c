ddca19279e9e6b50b101940d74f0a925ae6a4427
GLide KS <84994993+GLideKS@users.noreply.github.com>
2026-04-14 01:01:51 -0400

    menubgcolor uses a pre-defined color instead of giving a color index

--- src/r_main.c
+++ src/r_main.c
@@ -136,7 +136,13 @@ static CV_PossibleValue_t fov_cons_t[] = {{MINFOV*FRACUNIT, "MIN"}, {MAXFOV*FRAC
 static CV_PossibleValue_t translucenthud_cons_t[] = {{0, "MIN"}, {10, "MAX"}, {0, NULL}};
 static CV_PossibleValue_t maxportals_cons_t[] = {{0, "MIN"}, {12, "MAX"}, {0, NULL}}; // lmao rendering 32 portals, you're a card
 static CV_PossibleValue_t homremoval_cons_t[] = {{0, "No"}, {1, "Yes"}, {2, "Flash"}, {0, NULL}};
-static CV_PossibleValue_t menubg_cons_t[] = {{0, "MIN"}, {255, "MAX"}, {0, NULL}};
+static CV_PossibleValue_t menubg_cons_t[] = {
+	{15, "White"}, {21, "Gray"}, {28, "Black"}, {47, "Red"}, {15, "White"},
+	{58, "Creamsicle"}, {63, "Orange"}, {71, "Gold"}, {79, "Yellow"}, {95, "Moss"},
+	{109, "Green"}, {127, "Aqua"}, {138, "Sky"}, {159, "Blue"}, {169, "Purple"},
+	{127, "Aqua"}, {175, "Steel"}, {187, "Magenta"}, {199, "Lavender"},
+	{207, "Rosy"}, {215, "Lilac"}, {238, "Brown"}, {251, "Beige"}
+};
 
 static void R_SetFov(fixed_t playerfov);
 
@@ -151,7 +157,7 @@ void SendWeaponPref2(void);
 consvar_t cv_tailspickup = CVAR_INIT ("tailspickup", "On", CV_NETVAR|CV_ALLOWLUA, CV_OnOff, NULL);
 consvar_t cv_chasecam = CVAR_INIT ("chasecam", "On", CV_CALL, CV_OnOff, ChaseCam_OnChange);
 consvar_t cv_chasecam2 = CVAR_INIT ("chasecam2", "On", CV_CALL, CV_OnOff, ChaseCam2_OnChange);
-consvar_t cv_menubgcolor = CVAR_INIT ("menubgcolor", "159", CV_SAVE|CV_CALL|CV_CLIENT, menubg_cons_t, NULL);
+consvar_t cv_menubgcolor = CVAR_INIT ("menubgcolor", "Blue", CV_SAVE|CV_CALL|CV_CLIENT, menubg_cons_t, NULL);
 consvar_t cv_flipcam = CVAR_INIT ("flipcam", "No", CV_SAVE|CV_CALL|CV_NOINIT, CV_YesNo, FlipCam_OnChange);
 consvar_t cv_flipcam2 = CVAR_INIT ("flipcam2", "No", CV_SAVE|CV_CALL|CV_NOINIT, CV_YesNo, FlipCam2_OnChange);
 consvar_t cv_ringracers_quakes = CVAR_INIT ("rr_quakes", "No", CV_SAVE|CV_CALL|CV_CLIENT, CV_YesNo, NULL);

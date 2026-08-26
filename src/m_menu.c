4e00fc44258cb07cb3024085d78d87d8c58d0925
luigi-budd <90123055+luigi-budd@users.noreply.github.com>
2026-08-26 16:11:07 -0500

    Update server list menu again

--- src/m_menu.c
+++ src/m_menu.c
@@ -11817,7 +11817,7 @@ static void M_DrawConnectMenu(void)
 		if (Net_IsNodeIPv6(serverlist[slindex].node))
 			V_DrawSmallString(currentMenu->x+248, S_LINEY(i)+8, globalflags, "\x84" "IPv6");
 		if (serverlist[slindex].info.refusereason == REFUSE_BANNED) // probably banned or something. Probably.
-			V_DrawSmallString(currentMenu->x+248, S_LINEY(i)+4, globalflags &~V_TRANSLUCENT, "\x85" "X");
+			V_DrawSmallString(currentMenu->x+270, S_LINEY(i)+4, globalflags &~V_TRANSLUCENT, "\x85" "X");
 
 		V_DrawSmallString(currentMenu->x + 270, S_LINEY(i) + 8,
 			globalflags,

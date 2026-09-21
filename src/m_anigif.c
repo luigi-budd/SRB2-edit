d4633a80bc8fd08967e567b147e15ea1b3a12bc7
luigi-budd <90123055+luigi-budd@users.noreply.github.com>
2026-09-21 00:20:41 -0500

    Update default GIF max size

--- src/m_anigif.c
+++ src/m_anigif.c
@@ -47,7 +47,7 @@ consvar_t cv_gif_optimize = CVAR_INIT ("gif_optimize", "On", CV_SAVE, CV_OnOff, 
 consvar_t cv_gif_downscale =  CVAR_INIT ("gif_downscale", "On", CV_SAVE, CV_OnOff, NULL);
 consvar_t cv_gif_dynamicdelay = CVAR_INIT ("gif_dynamicdelay", "On", CV_SAVE, gif_dynamicdelay_cons_t, NULL);
 consvar_t cv_gif_localcolortable =  CVAR_INIT ("gif_localcolortable", "On", CV_SAVE, CV_OnOff, NULL);
-consvar_t cv_gif_maxsize =  CVAR_INIT ("gif_maxsize", "10", CV_SAVE|CV_CLIENT, gif_maxsize_cons_t, NULL);
+consvar_t cv_gif_maxsize =  CVAR_INIT ("gif_maxsize", "20", CV_SAVE|CV_CLIENT, gif_maxsize_cons_t, NULL);
 consvar_t cv_gif_rolling_buffer =  CVAR_INIT ("gif_rolling", "Off", CV_SAVE|CV_CLIENT, CV_OnOff, NULL);
 
 #ifdef HAVE_ANIGIF
